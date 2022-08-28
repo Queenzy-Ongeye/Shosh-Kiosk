0fe725d92d959d194747775868a288366c6e1d89
Queen-02 <queenzyblack176@gmail.com>
2022-08-28 19:22:51 +0300

    add fonts added

--- src/User.js
+++ src/User.js
@@ -1,37 +1,89 @@
 import React from 'react';
+import { FaFacebook } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import images from "./images/girl.png"
 import "./User.css"
 
 const User = () => {
     return (
         <div className="user">
-            <div className='main'>
-                <div className='eddie'>
-                    <h3>EddieShop</h3>
+            <div className='card'>
+                <div className='main'>
+                    <div className='eddie'>
+                        <h3>EddieShop</h3>
+                    </div>
+                    <div className='sign'>
+                        <p>Dont have an account? <span>Sign Up</span></p>
+                    </div>
                 </div>
-                <div className='sign'>
-                    <p>Dont have an account? <span>Sign Up</span></p>
+                <div className='form'>
+                    <div className='sec-one'>
+                        <h2>Welcome Back</h2>
+                        <div className='sec-two'>
+                            <p>Your email</p>
+                            <input type="text" placeholder="name@domain.com" className='email' />
+
+                            <p>Password</p>
+                            <input type="text" placeholder="at least 8 characters" className='password' />
+
+                            <div className='sec-three'>
+                                <div className='input'>
+                                    <input type="checkbox" /> Keep me logged in
+                                </div>
+                                <div>
+                                    <p className='pass'>Forgot password?</p>
+                                </div>
+                            </div>
+
+                            <button type="submit" className="btn btn-primary">Login</button>
+                            <p className="or">or</p>
+                            <div className="icons">
+                                <FaFacebook className="icon" />
+                                <FaGoogle className="icon" />
+                                <FaInstagram className="icon" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="side-img">
+                        <img src={images} alt="Logo" />
+                    </div>
+
                 </div>
             </div>
-            <div id='form'>
-                <div id='sec-one'>
-                    <h2>Welcome Back</h2>
-                    <div id='sec-two'>
-                        <p>Your email</p>
-                        <input type="text" placeholder="name@domain.com" className='email' />
 
-                        <p>Password</p>
-                        <input type="text" placeholder="at least 8 characters" className='password' />
 
-                        <div className='sec-three'>
-                            <input type="checkbox" /> Keep me logged in
-                            <p>Forgot password?</p>
-                        </div>
+            <div className="container">
+                <h2>Fonts</h2>
 
-                        <button type="submit" className="btn btn-primary">Login</button>
-                        <p>or</p>
-                        <div className="icons">
-                            
-                        </div>
+                <div className="letters">
+                    <div className="one">
+                        <h4>Aa</h4>
+                        <p className="robo">Roboto Regular</p>
+                    </div>
+
+                    <div className="two">
+                        <h4>Aa</h4>
+                        <p className="robo"> Roboto Medium</p>
+                    </div>
+                </div>
+
+                <h2>Colors</h2>
+                <div className="colors">
+                    <div className="color">
+                        <div className="color-two"></div>
+                        #FDE19D
+                    </div>
+
+                    <div className="color">
+                        <div className="color-three"></div>
+                        #469DA4
+                    </div>
+
+                    <div className="color-four">
+                        <div className="color-four"></div>
+                        #6B6B6B
                     </div>
                 </div>
 
